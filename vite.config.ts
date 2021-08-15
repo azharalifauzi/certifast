cd773363d0d6df9132e2e1a22b9ab50719540958
azharalifauzi <azharalifauzi@gmail.com>
2021-08-15 17:50:31 +0700

    remove vite-plugin-rsw from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import ViteRsw from 'vite-plugin-rsw';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    tsconfigPaths(),
-    ViteRsw({
-      crates: ['core-certifast'],
-    }),
-  ],
+  plugins: [reactRefresh(), tsconfigPaths()],
 });
