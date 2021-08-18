e4d595092a625db409208989a55122b598a172a5
azharalifauzi <azharalifauzi@gmail.com>
2021-08-18 21:01:55 +0700

    add google analytics using gtag.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { injectHtml } from 'vite-plugin-html';
+import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  // @ts-ignore
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      reactRefresh(),
+      tsconfigPaths(),
+      injectHtml({
+        injectData: {
+          gtag_id: env.VITE_GTAG_ID,
+        },
+      }),
+    ],
+  };
 });
