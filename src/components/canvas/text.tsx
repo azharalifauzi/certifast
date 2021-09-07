b19fc08eb5f2c68ee2ad1357f3209a1e0c464e71
azharalifauzi <azharalifauzi@gmail.com>
2021-09-07 15:14:29 +0700

    dynamic text labeling functionality

--- src/components/canvas/text.tsx
+++ src/components/canvas/text.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useOutsideClick } from '@chakra-ui/react';
 import {
   canvasObjects,
   activeToolbar as activeToolbarAtom,
@@ -10,7 +10,7 @@ import {
 } from 'gstates';
 import { useAtom } from 'jotai';
 import { useAtomValue, useUpdateAtom } from 'jotai/utils';
-import React, { useEffect, useState, memo } from 'react';
+import React, { useEffect, useState, memo, useRef } from 'react';
 import { useMemo } from 'react';
 import { useMount, useMeasure } from 'react-use';
 import WebFont from 'webfontloader';
@@ -23,6 +23,7 @@ interface CanvasTextProps {
 
 const CanvasText: React.FC<CanvasTextProps> = ({ id }) => {
   const [textRef, { height, width }] = useMeasure<HTMLDivElement>();
+  const inputRef = useRef<HTMLInputElement>(null);
   const [zoom] = useAtom(zoomCanvas);
   const [cObjects, setCObjects] = useAtom(canvasObjects);
   const [activeToolbar, setActiveToolbar] = useAtom(activeToolbarAtom);
@@ -35,8 +36,23 @@ const CanvasText: React.FC<CanvasTextProps> = ({ id }) => {
   const [triggerMove, setTriggerMove] = useState<boolean>(false);
   const [resize, setResize] = useState<boolean>(false);
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const [isEdit, setEdit] = useState<boolean>(false);
+  const [editCache, setEditCache] = useState({ initHeight: 0, initWidth: 0, initText: '' });
 
   const { data: textData } = useMemo(() => cObjects[id], [id, cObjects]);
+
+  useOutsideClick({
+    ref: inputRef,
+    handler: () => {
+      if (!textData.text)
+        setCObjects((obj) => {
+          const newObj = { ...obj };
+          newObj[selected].data.text = editCache.initText;
+          return newObj;
+        });
+      setEdit(false);
+    },
+  });
 
   useMount(() => {
     WebFont.load({
@@ -137,106 +153,169 @@ const CanvasText: React.FC<CanvasTextProps> = ({ id }) => {
       }
     };
 
-    window.addEventListener('keydown', handleDelete);
+    window.addEventListener('keydown', handleDelete, { capture: false });
 
     return () => window.removeEventListener('keydown', handleDelete);
   }, [id, selected, setCObjects, setSelected, setDynamicInputText]);
 
+  const handleChangeText: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCObjects((obj) => {
+      const newObj = { ...obj };
+      newObj[selected].data.text = e.target.value;
+      return newObj;
+    });
+  };
+
+  const chEdit = useMemo(() => {
+    const text = textData.text;
+    let p1 = text.length - 1;
+    let ch = 0;
+    while (text[p1] === ' ') {
+      ch++;
+      p1--;
+    }
+    return ch;
+  }, [textData.text]);
+
   return (
-    <Box
-      fontFamily={textData.family}
-      fontWeight={textData.weight}
-      _hover={{
-        textDecoration: 'underline',
-        textDecorationColor: 'blue.400',
-        textDecorationThickness: '2px',
-      }}
-      style={{
-        top: textData.y,
-        left: textData.x,
-      }}
-      zIndex="10"
-      ref={textRef}
-      color={textData.color}
-      userSelect="none"
-      position="absolute"
-      border="2px solid"
-      lineHeight="1.0"
-      borderColor={selected === id ? 'blue.400' : 'transparent'}
-      fontSize={textData.size * zoom}
-      onClick={() => {
-        if (spaceKey) return;
-        if (activeToolbar === 'move') setSelected(id);
-      }}
-      onMouseDown={(e) => {
-        if (spaceKey) return;
-        if (activeToolbar === 'move') setSelected(id);
-        setMousePos({
-          x: e.clientX - textData.x,
-          y: e.clientY - textData.y,
-        });
-        if (resize) {
-          setActiveToolbar('resize');
-        } else {
-          setTriggerMove(true);
-          setActiveToolbar('move');
-        }
-      }}
-      onMouseMove={() => {
-        if (activeToolbar === 'move' && resize) setResize(false);
-      }}
-    >
-      {selected === id ? (
+    <>
+      {isEdit ? (
         <Box
-          style={{
-            height: 10,
-            width: 10,
-            right: -6,
-            bottom: -6,
-          }}
+          as="input"
           position="absolute"
-          borderRadius="50%"
-          background="white"
-          border="1px solid"
+          zIndex="11"
+          background="transparent"
+          lineHeight="1.0"
+          border="2px solid"
           borderColor="blue.400"
-          _hover={{
-            cursor: 'nw-resize',
+          style={{
+            width: `calc(${width + 4}px + ${chEdit}ch)`,
+            height: !textData.text ? editCache.initHeight + 4 : height + 4,
           }}
-          onMouseOver={() => {
-            setResize(true);
-          }}
-          onMouseLeave={() => {
-            if (activeToolbar === 'move') setResize(false);
-          }}
-          onMouseMove={(e) => e.stopPropagation()}
+          _focus={{ outline: 'none' }}
+          _selection={{ background: 'lightblue' }}
+          onKeyDown={(e) => e.stopPropagation()}
+          onKeyPress={(e) => e.stopPropagation()}
+          onChange={handleChangeText}
+          fontFamily={textData.family}
+          fontWeight={textData.weight}
+          value={textData.text}
+          ref={inputRef}
+          top={textData.y}
+          left={textData.x}
+          color={textData.color}
+          fontSize={textData.size * zoom}
         />
       ) : null}
-      {selected === id ? (
-        <>
+      <Box
+        opacity={isEdit ? 0 : 1}
+        fontFamily={textData.family}
+        fontWeight={textData.weight}
+        _hover={{
+          textDecoration: 'underline',
+          textDecorationColor: 'blue.400',
+          textDecorationThickness: '2px',
+        }}
+        style={{
+          top: textData.y,
+          left: textData.x,
+        }}
+        zIndex="10"
+        ref={textRef}
+        color={textData.color}
+        userSelect="none"
+        position="absolute"
+        border="2px solid"
+        lineHeight="1.0"
+        borderColor={selected === id ? 'blue.400' : 'transparent'}
+        fontSize={textData.size * zoom}
+        onClick={() => {
+          if (spaceKey) return;
+          if (activeToolbar === 'move') setSelected(id);
+        }}
+        onMouseDown={(e) => {
+          if (spaceKey) return;
+          if (activeToolbar === 'move') setSelected(id);
+          setMousePos({
+            x: e.clientX - textData.x,
+            y: e.clientY - textData.y,
+          });
+          if (resize) {
+            setActiveToolbar('resize');
+          } else {
+            setTriggerMove(true);
+            setActiveToolbar('move');
+          }
+        }}
+        onMouseMove={() => {
+          if (activeToolbar === 'move' && resize) setResize(false);
+        }}
+        onDoubleClick={() => {
+          setEditCache({
+            initHeight: height,
+            initWidth: width,
+            initText: textData.text,
+          });
+          setEdit(true);
+          setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(0, textData.text.length - 1);
+            inputRef.current?.select();
+          }, 100);
+        }}
+      >
+        {selected === id ? (
           <Box
-            left={textData.align === 'left' ? '0' : textData.align === 'center' ? '50%' : '100%'}
-            top="50%"
-            transform="translate(-50%, -50%)"
+            style={{
+              height: 10,
+              width: 10,
+              right: -6,
+              bottom: -6,
+            }}
             position="absolute"
-          >
-            <GiCrosshair color="#4399E1" size={12 * zoom} />
-          </Box>
-          <Box
-            pos="absolute"
-            bottom="-20px"
-            transform="translateX(-50%)"
-            left="50%"
-            background="blue.400"
-            color="white"
-            fontSize="xs"
-            width="max-content"
-          >
-            {textData.width?.toFixed(0)} x {textData.height?.toFixed(0)}
-          </Box>
-        </>
-      ) : null}
-      {textData.text}
-    </Box>
+            borderRadius="50%"
+            background="white"
+            border="1px solid"
+            borderColor="blue.400"
+            _hover={{
+              cursor: 'nw-resize',
+            }}
+            onMouseOver={() => {
+              setResize(true);
+            }}
+            onMouseLeave={() => {
+              if (activeToolbar === 'move') setResize(false);
+            }}
+            onMouseMove={(e) => e.stopPropagation()}
+          />
+        ) : null}
+        {selected === id ? (
+          <>
+            <Box
+              left={textData.align === 'left' ? '0' : textData.align === 'center' ? '50%' : '100%'}
+              top="50%"
+              transform="translate(-50%, -50%)"
+              position="absolute"
+            >
+              <GiCrosshair color="#4399E1" size={12 * zoom} />
+            </Box>
+            <Box
+              pos="absolute"
+              bottom="-20px"
+              transform="translateX(-50%)"
+              left="50%"
+              background="blue.400"
+              color="white"
+              fontSize="xs"
+              width="max-content"
+            >
+              {textData.width?.toFixed(0)} x {textData.height?.toFixed(0)}
+            </Box>
+          </>
+        ) : null}
+        {textData.text}
+      </Box>
+    </>
   );
 };
 
